0886978ec97b6223320cc542babaee1ef7ba1303
qwerty451 <tvanheijst13@gmail.com>
2022-12-27 18:42:32 +0000

    Temp fix for ALLOWED_HOSTS bs

--- project/settings.py
+++ project/settings.py
@@ -27,8 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
-#ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
-ALLOWED_HOSTS = ['.localhost', '127.0.0.1', 'localhost', 'kwerti.nl', 'project.kwerti.nl', 'www.kwerti.nl', 'seashell-app-ubtbd.ondigitalocean.app']
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
